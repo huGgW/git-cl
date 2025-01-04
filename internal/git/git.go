32bcc416185288abde86fb9eec53cb784acddaca
huGgW <whjoon0225@naver.com>
2025-01-05 00:27:25 +0900

    feat: add GetLocalBranches function

--- internal/git/git.go
+++ internal/git/git.go
@@ -1,10 +1,50 @@
 package git
 
 import (
+	"bufio"
+	"bytes"
 	"context"
 	"fmt"
+	"io"
 	"os/exec"
+	"strings"
+
+	"github.com/huGgW/git-cl/pkg/iterator"
+	"github.com/huGgW/git-cl/pkg/iterator/scanner"
 )
+
+type LocalBranches struct {
+	Current string
+	All     []string
+}
+
+func parseLocalBranches(reader io.Reader) (LocalBranches, error) {
+	const wrapErrMsg = "failed to parse local branches"
+
+	branches := LocalBranches{}
+	sc := bufio.NewScanner(reader)
+	scSeqErr := iterator.Map2(
+		scanner.LineSeq(sc),
+		func(line string, err error) (string, error) {
+			return strings.TrimSpace(line), err
+		},
+	)
+
+	for line, err := range scSeqErr {
+		if err != nil {
+			return LocalBranches{}, fmt.Errorf("%s: %w", wrapErrMsg, err)
+		}
+
+		if strings.HasPrefix(line, "* ") {
+			line = strings.TrimPrefix(line, "* ")
+			branches.Current = line
+		}
+
+		branches.All = append(branches.All, line)
+	}
+
+	return branches, nil
+}
 
 func FetchAll(ctx context.Context) error {
 	const wrapErrMsg = "failed to fetch repository"
@@ -14,4 +54,21 @@ func FetchAll(ctx context.Context) error {
 	}
 
 	return nil
+}
+
+func GetLocalBranches(ctx context.Context) (LocalBranches, error) {
+	const wrapErrMsg = "failed to get local branches"
+
+	cmd := exec.CommandContext(ctx, "git", "branch", "--list")
+	out, err := cmd.Output()
+	if err != nil {
+		return LocalBranches{}, fmt.Errorf("%s: %w", wrapErrMsg, err)
+	}
+
+	localBranches, err := parseLocalBranches(bytes.NewReader(out))
+	if err != nil {
+		return LocalBranches{}, fmt.Errorf("%s: %w", wrapErrMsg, err)
+	}
+
+	return localBranches, nil
 }

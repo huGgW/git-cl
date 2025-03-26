662d3558835a887d0e65061ca5bb5b4a8e7a3c81
우혁준 (Logan) <whjoon0225@naver.com>
2025-03-26 23:20:12 +0900

    refactor: extract current exclude filter (#12)
    * refactor: extract current exclude filter to viewer.go
    
    * docs: add comment for reason of using current exclude filter

--- internal/git/gogit_viewer.go
+++ internal/git/gogit_viewer.go
@@ -64,12 +64,11 @@ func (g *gogitViewer) GetLocalBranches(ctx context.Context) (LocalBranches, erro
 }
 
 func (g *gogitViewer) FilterLocalBranches(ctx context.Context, branches LocalBranches, filters ...Filter) []Branch {
-	currentExcludeFilter := func(branch Branch) bool {
-		return !branch.IsCurrent
-	}
-
 	branchSeq := slices.Values(branches.Branches)
+
+	// add current exclude filter because branch deletion cannot delete the current branch
 	branchSeq = iterator.Filter(branchSeq, currentExcludeFilter)
+
 	for _, filter := range filters {
 		branchSeq = iterator.Filter(branchSeq, filter)
 	}
